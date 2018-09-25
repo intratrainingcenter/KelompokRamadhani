b2a97782e259e2202c19cf63684165c0abf98724
Rama <kazureisuke@gmail.com>
2018-09-25 11:08:50 +0700

    perbaiki model guru

--- app/guru.php
+++ app/guru.php
@@ -7,11 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class guru extends Model
 {
     protected $table = 'gurus';
-<<<<<<< HEAD
-    protected $fillable = ['kode_guru','nama_guru','mapel'];
-=======
     protected $primaryKey = 'id_guru';
     protected $fillable = ['id_guru','kode_guru','nama_guru','kode_mapel'];
 
->>>>>>> mapel&siswa
 }
