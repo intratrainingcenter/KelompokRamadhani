89281b2aa5c110ec98d2554367f0dac7d694f8f2
Ramadhani <kazureisuke@gmail.com>
2018-09-27 02:25:49 +0700

    build absensi

--- resources/views/pages/content/skl/detailabsen.blade.php
+++ resources/views/pages/content/skl/detailabsen.blade.php
@@ -6,35 +6,55 @@
   <div class="col-xs-12">
     <div class="box">
       <div class="box-header">
-        <h3 class="box-title">Tabel Absensi</h3>
        <div class="box-tools">
     		<div class="input-group input-group-sm" style="width: 150px;">
+            {{-- <button type="button" class="btn-lg btn-success fa fa-plus-square add" title="Add Siswa" data-toggle="modal" data-target="#modal-success"> <label>Confirm </label></button>  --}}
       		<div class="input-group-btn">
       		</div>
     		</div>
   	   </div>
       </div>
       <!-- /.box-header -->
+      
+
+
       <div class="box-body table-responsive no-padding">
         <table class="table table-hover">
           <tr>
             <th>No</th>
-            <th>Kelas</th>
-            <th>Wali Kelas</th>
-            <th>Action</th>
+            <th>NIS</th>
+            <th>Nama Siswa</th>
+            <th>Keterangan</th>
+           
           </tr>
-            {{-- @foreach($detail as $class)
+            @foreach($detail as $class)
           <tr>
             <td>{{$loop->iteration}}</td>
-            <td>{{$class->nama_kelas}}</td>
-            <td>{{$class->detailguru->nama_guru}}</td>
+            <td>{{$class->NIS}}</td>
+            <td>{{$class->nama_siswa}}</td>
             <td>
-            	<button type="button" class="btn-lg btn-info"><li class="fa fa-search-plus"></li></button>
-            	<button type="button" class="btn-lg btn-success"><li class="fa fa-list-alt"></li></button>
+          {!! Form::open(['route' => 'adddetail' , 'method' => 'post'])!!}
+
+                <div class="col-sm-8">
+                    <select class="form-control" name="ket">
+                    <option value="masuk">masuk</option>
+                    <option value="tanpa_keterangan">Alpha</option>
+                    <option value="ijin">Ijin</option>
+                    <option value="sakit">Sakit</option>
+                  </select>
+                </div>
             </td>
           </tr>
-            @endforeach --}}
+            @endforeach
         </table>
+        @foreach($detail as $class)
+        <input type="hidden" name="NIS" value="{{$class->NIS}}">
+        <input type="hidden" name="kode_kelas" value="{{$class->kode_kelas}}">
+        @endforeach
+        <a href="{{route('absen.index')}}" type="button" class="btn btn-danger" title="Back"> <label>Back</label></a>                                 
+        <button type="submit" class="btn btn-success pull-right">Submit</button>
+        {!! Form::close() !!}
+        
       </div>
       <!-- /.box-body -->
     </div>
