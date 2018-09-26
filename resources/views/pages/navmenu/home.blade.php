a2144a1c9399a885e8dd6398a4806ed86506accc
obed ridho <deboabilla@gmail.com>
2018-09-26 10:03:11 +0700

    update icon in menu
    -

--- resources/views/pages/navmenu/home.blade.php
+++ resources/views/pages/navmenu/home.blade.php
@@ -3,12 +3,12 @@
 @section('footer') Created at 2018 @endsection
 @section('navmenu')
 <li class="active treeview">
-    <li><a href="{{route('siswa.index')}}"><i class="fa fa-circle-o"></i> Siswa </a></li>
-    <li><a href="{{route('kelas.index')}}"><i class="fa fa-circle-o"></i> Kelas </a></li>
-    <li><a href="{{route('absen.index')}}"><i class="fa fa-circle-o"></i> Absensi </a></li>
-    <li><a href="{{route('piket.index')}}"><i class="fa fa-circle-o"></i> Piket </a></li>
-    <li><a href="{{route('mapel.index')}}"><i class="fa fa-circle-o"></i> Mapel </a></li>
-    <li><a href="{{route('guru.index')}}"><i class="fa fa-circle-o"></i> Guru </a></li>
+    <li><a href="{{route('siswa.index')}}"><i class="fa  fa-child"></i> Siswa </a></li>
+    <li><a href="{{route('kelas.index')}}"><i class="fa  fa-institution"></i> Kelas </a></li>
+    <li><a href="{{route('absen.index')}}"><i class="fa  fa-list"></i> Absensi </a></li>
+    <li><a href="{{route('piket.index')}}"><i class="fa fa-trash-o"></i> Piket </a></li>
+    <li><a href="{{route('mapel.index')}}"><i class="fa  fa-table"></i> Mapel </a></li>
+    <li><a href="{{route('guru.index')}}"><i class="fa fa-group"></i> Guru </a></li>
  
   </li> 
 @endsection
