9d97945b421a4bd768df8f89a67573703d3e493f
Rama <kazureisuke@gmail.com>
2018-09-26 21:17:12 +0700

    mapel fix

--- app/siswa.php
+++ app/siswa.php
@@ -8,7 +8,7 @@ class siswa extends Model
 {
     protected $table = 'siswas';
     protected $primaryKey = 'id_siswa';
-    protected $fillable = ['id_siswa','NIS','nama_siswa','kode_kelas','alamat','tgl_lahir'];
+    protected $fillable = ['id_siswa','NIS','nama_siswa','kode_kelas','kode_piket','alamat','jk'];
 
     public function detailguru()
     {
