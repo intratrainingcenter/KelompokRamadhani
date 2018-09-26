bfd3cbaf2d22686fbee4c6a4b0a0938fda267774
obed ridho <deboabilla@gmail.com>
2018-09-26 21:18:22 +0700

    create table piket
    -

--- app/jadpiket.php
+++ app/jadpiket.php
@@ -9,4 +9,9 @@ class jadpiket extends Model
     protected $table = 'jadpikets';
     protected $primarykey = 'id';
     protected $fillable = ['id','kode_piket','hari'];
+
+    public function detailsiswa()
+    {
+    	return $this->hasmany('App\siswa','kode_piket','kode_piket');
+    }
 }
